d12f812eaa9ac4eca3ee513424b13d6a38b8640f
JAECMO <dr.jalal.abdul.hadi@gmail.com>
2023-04-18 17:27:58 -0400

    5th commit

--- Exercises/Lucky_Sevens/src/main/java/com/jah/lucky_sevens/Lucky_Sevens.java
+++ Exercises/Lucky_Sevens/src/main/java/com/jah/lucky_sevens/Lucky_Sevens.java
@@ -14,113 +14,108 @@ import java.util.Scanner;
  * @author drjal
  */
 public class Lucky_Sevens {
+
     static Random rGen = new Random();
     static int dice1;
     static int dice2;
-    
-    public static void main(String [] args) {
-        
+
+    public static void main(String[] args) {
+
         Scanner input = new Scanner(System.in);
         String moneyString;
         int moneyInt;
-        int [] arrayMoney;
-        int [] arrayTemp;
+        int[] arrayMoney;
+        int[] arrayTemp;
         int max;
-        
-       
+
         while (true) {
 
-           try {
-               System.out.println("How many dollars do you have? ");
-               moneyString = input.nextLine();
-               moneyInt  = Integer.parseInt(moneyString);
-               
+            try {
+                System.out.println("How many dollars do you have? ");
+                moneyString = input.nextLine();
+                moneyInt = Integer.parseInt(moneyString);
+
                 if (moneyInt < 0) {
-                        throw new NumberFormatException("Invalid input. Please only enter a positive integer.");
-                    }
-               break;
-           }catch (NumberFormatException e) { 
-                 System.out.println("Please enter an integer number of dollars");
-           }
-         
-         }//while
-         
-          arrayMoney = arrayMoneyCal(moneyInt);
-          arrayTemp=buildTempArray(arrayMoney);
-          max = findMax(arrayTemp);
-          displayArray(arrayMoney,max);
+                    throw new NumberFormatException("Invalid input. Please only enter a positive integer.");
+                }
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("Please enter an integer number of dollars");
+            }
+
+        }//while
+
+        arrayMoney = arrayMoneyCal(moneyInt);
+        arrayTemp = buildTempArray(arrayMoney);
+        max = findMax(arrayTemp);
+        displayArray(arrayMoney, max);
     }//main
-    
-    public static int [] arrayMoneyCal(int moneyInt ){
-        
+
+    public static int[] arrayMoneyCal(int moneyInt) {
+
         int[] arrayMoneyCal = new int[0];
         int diceSum = 0;
         int i = 0;
-        
-                
+
         while (moneyInt != 0) {
 
             dice1 = rGen.nextInt(6) + 1;
             dice2 = rGen.nextInt(6) + 1;
-          
+
             diceSum = dice1 + dice2;
-            
-   //System.out.println(dice1 + " " + dice2);
-   
+
+            //System.out.println(dice1 + " " + dice2);
             if (diceSum == 7) {
                 moneyInt += 4;
             } else {
                 moneyInt -= 1;
             }
-           // System.out.println(moneyInt);
-           arrayMoneyCal=GrowArray(arrayMoneyCal,1);
-            arrayMoneyCal[i]=moneyInt;
+            // System.out.println(moneyInt);
+            arrayMoneyCal = GrowArray(arrayMoneyCal, 1);
+            arrayMoneyCal[i] = moneyInt;
             i++;
-            
-          
+
         }//while
-        
-           for (int j = 0; j < arrayMoneyCal.length; j++) {
 
-            System.out.println(arrayMoneyCal[j]);
-        }
-
+//           for (int j = 0; j < arrayMoneyCal.length; j++) {
+//
+//            System.out.println(arrayMoneyCal[j]);
+//        }
         return arrayMoneyCal;
     }
-    
-    public static void displayArray(int [] array1, int max){
-    
+
+    public static void displayArray(int[] array, int max) {
+
         int rolls = 0;
 
-        System.out.println("You are broke after " + array1.length + " rolls.");
-        
+        System.out.println("You are broke after " + array.length + " rolls.");
 
-        for (int i = 0; i < array1.length; i++) {
+        for (int i = 0; i < array.length; i++) {
 
-            if (array1[i] == max) {
-                rolls = i+1;
+            if (array[i] == max) {
+                rolls = i + 1;
             }
         }
 
         System.out.println("You should have quit after " + rolls + " rolls when you had $" + max + ".");
-      
+
     }
-    
-    public static int[] buildTempArray(int [] array){
-        
-        int [] tempArray = new int [array.length];
-        
-    for (int i=0;i<array.length;i++){
-    
-        tempArray[i]=array[i];
-    
-    }
-        
+
+    public static int[] buildTempArray(int[] array) {
+
+        int[] tempArray = new int[array.length];
+
+        for (int i = 0; i < array.length; i++) {
+
+            tempArray[i] = array[i];
+
+        }
+
         return tempArray;
     }
-    
+
     public static int findMax(int[] array) {
-        
+
         int max = 0;
 
         for (int i = 0; i < array.length - 1; i++) {
@@ -136,19 +131,17 @@ public class Lucky_Sevens {
 
         return max;
     }
-    
-       public static int[] GrowArray(int[] original, int howManyMoreElements)
-{
-    int[] newArray = new int[original.length + howManyMoreElements];
 
-    for (int i = 0; i < original.length; i++)
-    {
-        // copy the element at the current index
-        // from original to newArray
-        newArray[i] = original[i];
-        //System.out.println(newArray[i]);
+    public static int[] GrowArray(int[] original, int howManyMoreElements) {
+        int[] newArray = new int[original.length + howManyMoreElements];
+
+        for (int i = 0; i < original.length; i++) {
+            // copy the element at the current index
+            // from original to newArray
+            newArray[i] = original[i];
+            //System.out.println(newArray[i]);
+        }
+
+        return newArray;
     }
-
-    return newArray;
-}
 }
---
+++ Exercises/SummativeSums/src/main/java/com/jah/summativesums/SummativeSums.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.jah.summativesums;
+
+/**
+ *
+ * @author drjal
+ */
+public class SummativeSums {
+    static int counter=0;
+    public static void main(String[] args) {
+        int [] arraySum;
+        
+        int[] array1 = {1, 90, -33, -55, 67, -16, 28, -55, 15};
+        int[] array2 = {999, -60, -77, 14, 160, 301};
+        int[] array3 = {10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130,
+            140, 150, 160, 170, 180, 190, 200, -99};
+        
+        arraySum=arraySum(array1);
+        displayArray(arraySum);
+        
+        arraySum=arraySum(array2);
+        displayArray(arraySum);
+        
+        arraySum=arraySum(array3);
+        displayArray(arraySum);
+    }
+
+    public static int [] arraySum(int [] array){
+    int arrayTotal = 0;
+    counter++;
+    
+    for (int i = 0; i < array.length; i++) {
+        arrayTotal+=array[i];
+    }
+    
+   int [] arraySum = {arrayTotal,counter};
+    
+    return arraySum;
+    }
+       public static void displayArray(int [] arraySum){
+
+        System.out.println("#" + arraySum[1] + " Array Sum: "+ arraySum[0]);
+     
+      
+    }
+}
